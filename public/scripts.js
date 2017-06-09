7a8b9c4c570979f3d3825573667aa69c620e4c2d
Luis Diaz <luis@linuxmail.org>
2017-06-09 01:28:32 -0700

    Previously had reassigned number to number = number.toString(). Instead assigned string number to var numberString for clarity

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 // tiles
 var green = document.querySelector('#green');
 var red = document.querySelector('#red');
@@ -18,6 +20,7 @@ var gameContainer = document.querySelector('#game-container');
 var count = 0;
 var currentSequence = [];
 var userSequence = [];
+var strictMode = 'off';
 
 // audio elements
 var greenAudio = document.createElement('audio');
@@ -57,25 +60,25 @@ function next() {
 function generateNumber() {
   // generate number from 1-4
   var number = Math.floor(Math.random() * 4) + 1;
-  number = number.toString();
-  currentSequence.push(number);
+  numberString = number.toString();
+  currentSequence.push(numberString);
   lightSequence();
 }
 
 function lightSequence() {
   // light up each tile by data-id
-  currentSequence.forEach(function (number, index) {
+  currentSequence.forEach(function (numberString, index) {
     index += 1; // increment for use in setTimeout/delay time
-    var tileLit = document.querySelectorAll('[data-id=\'' + number + '\']')[0];
-    lightUp(tileLit, index, number);
+    var tileLit = document.querySelectorAll('[data-id=\'' + numberString + '\']')[0];
+    lightUp(tileLit, index, numberString);
   });
 }
 
 // called by lightSequence and lights up the tile, uses index for spacing out the timing and plays the audio through data-id attribute
-function lightUp(tile, index, number) {
+function lightUp(tile, index, numberString) {
   setTimeout(function () {
     tile.style.opacity = 0.5;
-    audioPlay(number);
+    audioPlay(numberString);
   }, index * 1000); // adds delayed affect for tile light up
 
   setTimeout(function () {
@@ -84,9 +87,9 @@ function lightUp(tile, index, number) {
 }
 
 // handles audio file to be played based on data-id value of user's choice
-function audioPlay(number) {
+function audioPlay(numberString) {
 
-  switch (number) {
+  switch (numberString) {
 
     case '1':
       greenAudio.play();
@@ -116,8 +119,7 @@ function userClicks(event) {
 // if the choice is correct then push it to userSequence
 function checkChoices(choice) {
   console.log('choice id:', choice);
-
-  choice = choice.toString();
+  console.log(typeof choice === 'undefined' ? 'undefined' : _typeof(choice));
 
   if (choice === currentSequence[count]) {
     // here is why count is not initially incremented and remains at 0 for the first try
--- src/scripts.js
+++ src/scripts.js
@@ -16,6 +16,7 @@ var gameContainer = document.querySelector('#game-container');
 var count = 0;
 var currentSequence = [];
 var userSequence = [];
+var strictMode = 'off';
 
 // audio elements
 var greenAudio = document.createElement('audio');
@@ -53,24 +54,24 @@ function next() {
 
 function generateNumber() { // generate number from 1-4
   var number = Math.floor(Math.random() * 4) + 1;
-  number = number.toString();
-  currentSequence.push(number);
+  numberString = number.toString();
+  currentSequence.push(numberString);
   lightSequence();
 }
 
 function lightSequence() { // light up each tile by data-id
-  currentSequence.forEach((number, index) => {
+  currentSequence.forEach((numberString, index) => {
     index +=1; // increment for use in setTimeout/delay time
-    var tileLit = document.querySelectorAll(`[data-id='${number}']`)[0];
-    lightUp(tileLit, index, number);
+    var tileLit = document.querySelectorAll(`[data-id='${numberString}']`)[0];
+    lightUp(tileLit, index, numberString);
   });
 }
 
 // called by lightSequence and lights up the tile, uses index for spacing out the timing and plays the audio through data-id attribute
-function lightUp(tile, index, number) {
+function lightUp(tile, index, numberString) {
   setTimeout(function(){
       tile.style.opacity = 0.5;
-      audioPlay(number);
+      audioPlay(numberString);
     }, index * 1000); // adds delayed affect for tile light up
 
   setTimeout(function(){
@@ -79,9 +80,9 @@ function lightUp(tile, index, number) {
 }
 
 // handles audio file to be played based on data-id value of user's choice
-function audioPlay(number) {
+function audioPlay(numberString) {
 
-  switch (number) {
+  switch (numberString) {
 
   case '1':
     greenAudio.play();
@@ -112,8 +113,7 @@ function userClicks(event) {
 // if the choice is correct then push it to userSequence
 function checkChoices(choice) {
   console.log('choice id:', choice);
-
-  choice = choice.toString();
+  console.log(typeof choice)
 
   if (choice === currentSequence[count]) { // here is why count is not initially incremented and remains at 0 for the first try
     userSequence.push(choice);
